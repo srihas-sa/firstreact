b685f3cee0ecad6004e96718360a4ef2c7625c67
srihas-sa <edupugantisrihas2003@gmail.com>
2023-05-29 13:54:03 +0000

    first Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const element = (
+  // Write your code here.
+  <div className="outside">
+    <h1>Congratulations</h1>
+    <div className="inside">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"></img>
+      <h1>Kiran V</h1>
+      <p>
+        jnfa jnfj bfhb ehbfhg bbgie<br></br>rgb bhbrgih g{" "}
+      </p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"></img>
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
